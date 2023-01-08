544e815c75347c4dea50bf4f2db5c96083bef6b7
slashack <funkcharles@hotmail.com>
2023-01-07 19:14:21 -0500

    Add operate function to access basic math function

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
 
 //Basic Math Functions
 function add(num1, num2) {
