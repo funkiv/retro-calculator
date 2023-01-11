1894bd1eee34fa2a5cb77fdfd4f0192c61a5866a
slashack <funkcharles@hotmail.com>
2023-01-10 20:43:50 -0500

    Add tweaks to code redundancy

--- script.js
+++ script.js
@@ -43,59 +43,54 @@ for(let item of calcButtons) {
         buttonPress(item.innerText);
     });
 }
-//expression variables
+//Global expression variables
 let num1 = "";
 let num2 = "";
 let operator = "";   
 let total = "";
 //Function called from event listener when any calc button is pressed
 function buttonPress(calcButton) { 
+    //Stops overflow on calc screen
     let isCountBelow = calcScreen.textContent.length < 12;  
-        //=
-        if((calcButton === "="||calcButton === "+"||calcButton === "-"||calcButton === "*"||calcButton === "/") && num1 !== "" && operator != "" && num2 !== ""){
-            total = operate(operator, num1, num2)
-            calcScreen.textContent = total;
-            num1 = total;
-            num2 = "";
-            if (calcButton !== "="){
-            operator = calcButton;
-            calcScreen.textContent += calcButton;
-            } else {
-                operator = "";
-            }
-            total = "";
-        //C
-        } else if (calcButton === "C"){
-            num1 = "";
-            num2 = "";
-            operator = "";
-            calcScreen.textContent = "0";
-        //%
-        } else if (calcButton === "%"){
-            num1 = num1 / "100";
-            calcScreen.textContent = num1
-        //+/-
-        } else if (calcButton === "+/-"){
-            //Toggles positive and negative
-            if (num1 < 0) {
-                num1 = Math.abs(num1);
-            } else {
-                num1 = -Math.abs(num1);
-            }
-            calcScreen.textContent = num1;
-        //num1
-        } else if (((!isNaN(Number(calcButton)) ||calcButton === ".") && operator === "") && isCountBelow) {
-            num1 += calcButton;
-            calcScreen.textContent = num1;
-        //operator
-        } else if(num1 !== "" && isNaN(Number(calcButton)) && calcButton !== "=" && calcButton !== ".") {
-            operator = calcButton;
-            calcScreen.textContent = num1 + operator;
-        //num2
-        } else if((operator !== "" && !isNaN(Number(calcButton)) || calcButton === ".") && isCountBelow) {
-            num2 += calcButton;
-            calcScreen.textContent = num1 + operator + num2;
-        }      
+            //=
+    if((calcButton === "="||calcButton === "+"||calcButton === "-"||calcButton === "*"||calcButton === "/") 
+    && num1 !== "" && operator != "" && num2 !== ""){
+        total = Math.round(operate(operator, num1, num2) * 100) / 100
+        num1 = total;
+        num2 = "";
+        operator = "";
+        if (calcButton !== "="){
+        operator = calcButton;
+        }
+    //C
+    } else if (calcButton === "C"){
+        num1 = "";
+        num2 = "";
+        operator = "";
+    //%
+    } else if (calcButton === "%"){
+        num1 = num1 / "100";
+    //+/-
+    } else if (calcButton === "+/-"){
+        //Toggles positive and negative
+        if (num1 < 0) {
+            num1 = Math.abs(num1);
+        } else {
+            num1 = -Math.abs(num1);
+        }
+    //num1
+    } else if (((!isNaN(Number(calcButton)) ||calcButton === ".") && operator === "") && isCountBelow) {
+        num1 += calcButton;
+    //operator
+    } else if(num1 !== "" && isNaN(Number(calcButton)) && calcButton !== "=" && calcButton !== ".") {
+        operator = calcButton;
+    //num2
+    } else if((operator !== "" && !isNaN(Number(calcButton)) || calcButton === ".") && isCountBelow) {
+        num2 += calcButton;
+    }
+    //Updates screen after button is pressed 
+    calcScreen.textContent = num1 + operator + num2;         
+    if (calcButton==="C") calcScreen.textContent = "0";
 }
 
 
